3de6aef4990651916ef4be6ae1f1b4dceecbbcbd
Bhupesh Bothra <bhupeshbothra@gmail.com>
2018-10-24 12:04:56 +0530

    adding files

---
+++ main.ts
@@ -0,0 +1,19 @@
+class Point{
+    x:number;
+    y:number;
+
+    constructor(x?:number, y?:number){
+        this.x = x;
+        this.y =y;
+
+    }
+   public drow (){
+        console.log('X : ' + this.x + ' Y: ' + this. y);
+    }
+}
+
+
+
+let point =new Point(5,6);
+
+point.drow();
